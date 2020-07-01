eae94f0ab4526042324120ec97dee3a803b170d6
TakahiroFujita <fujita@exidea.co.jp>
2020-07-01 16:52:21 +0900

    smarty用のディレクトリを作成

---
+++ Codeigniter/application/views/templates_c/055395d30c833b7f45739233c3a639127bc0ad58_0.file.test.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-07-01 05:30:22
+  from '/var/www/Codeigniter/application/views/templates/test.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5efc1f6e42fee9_86368025',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '055395d30c833b7f45739233c3a639127bc0ad58' => 
+    array (
+      0 => '/var/www/Codeigniter/application/views/templates/test.tpl',
+      1 => 1593581725,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5efc1f6e42fee9_86368025 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['test']->value;
+}
+}
