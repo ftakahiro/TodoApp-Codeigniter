eea9cb6b7f14807199a27f8e15c5759ab2f96422
TakahiroFujita <fujita@exidea.co.jp>
2020-08-13 13:12:59 +0900

    バックエンドにてデータ登録するためのルートを設定

--- Codeigniter/application/config/routes.php
+++ Codeigniter/application/config/routes.php
@@ -54,4 +54,5 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
-$route['home/getdata'] ='home/getdata';
+$route['home/getdata'] ='home/getData';
+$route['home/dataStore'] ='home/dataStore';
