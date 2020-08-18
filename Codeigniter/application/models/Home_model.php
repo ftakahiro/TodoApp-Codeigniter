1d9b8cd2d0e71f8a49705681f4992f1fd99baebf
TakahiroFujita <fujita@exidea.co.jp>
2020-08-18 17:33:22 +0900

    楽観的排他制御を実装

--- Codeigniter/application/models/Home_model.php
+++ Codeigniter/application/models/Home_model.php
@@ -14,6 +14,27 @@ class Home_model extends CI_Model {
         return $tasksAll;
     }
 
+    // 楽観ロック用にデータの中で最も新しいものを取得
+    public function getUpdatedAt()
+    {
+        $taskParent = $this->db->query('SELECT * FROM tasks_parent ORDER BY updated_at DESC LIMIT 1')->row();
+        $taskChild  = $this->db->query('SELECT * FROM tasks_child  ORDER BY updated_at DESC LIMIT 1')->row();
+
+        if (!isset($taskParent) && !isset($taskChild)) {
+            return 0;
+        } elseif (isset($taskParent->updated_at) && !isset($taskChild->updated_at)) {
+            return $taskParent->updated_at;
+        } elseif (!isset($taskParent->updated_at) && isset($taskChild->updated_at)) {
+            return $taskChild->updated_at;
+        } else {
+            if($taskParent->updated_at >= $taskChild->updated_at) {
+                return $taskParent->updated_at;
+            } else {
+                return $taskChild->updated_at;
+            }
+        }
+    }
+
     public function processData($tasksAllNew)
     {
         $this->load->library('form_validation');
@@ -62,6 +83,7 @@ class Home_model extends CI_Model {
                     'comment' => htmlspecialchars($taskParentNew['comment']),
                     'check_flag' => (int)$taskParentNew['check_flag'],
                     'delete_flag' => (int)$taskParentNew['delete_flag'],
+                    'updated_at' => date("Y-m-d H:i:s", time()),
                 ];
                 $this->db->where('id', $taskParentNew['id']);
                 $this->db->update('tasks_parent', $data);
@@ -81,6 +103,7 @@ class Home_model extends CI_Model {
                             'check_flag' => (int)$taskChildNew['check_flag'],
                             'delete_flag' =>(int)$taskChildNew['delete_flag'],
                             'parent_id' => (int)$taskChildNew['parent_id'],
+                            'updated_at' => date("Y-m-d H:i:s", time()),
                         ];
                         $this->db->insert('tasks_child', $data);
                     // 子タスク削除 and 更新
@@ -90,6 +113,7 @@ class Home_model extends CI_Model {
                             'comment' => htmlspecialchars($taskChildNew['comment']),
                             'check_flag' => (int)$taskChildNew['check_flag'],
                             'delete_flag' => (int)$taskChildNew['delete_flag'],
+                            'updated_at' => date("Y-m-d H:i:s", time()),
                         ];
                         $this->db->where('id', $taskChildNew['id']);
                         $this->db->update('tasks_child', $data);
